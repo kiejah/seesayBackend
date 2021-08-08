0d61feb0b99a6c251f16a77591dc260436a72bec
kiejah <kiejah@gmail.com>
2021-08-08 13:16:36 +0300

    -- first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email'=>'required|email',
+            'password'=>'required|string'
+ 
+        ]); 
+        //check email
+        $user = User::where('email',$fields['email'])->first(); 
+
+        //Check Password
+        if(!$user || !Hash::check($fields['password'],$user->password)){
+                return response([
+                    'message'=>'bad creds'
+                ],401);
+        }
+ 
+        $token = $user->createToken('mytoken')->plainTextToken;
+ 
+        $response = [
+            'user'=>$user,
+            'token'=>$token,
+            'password'=>bcrypt($fields['password']),
+            'message'=>'Login Successful'
+         ];
+
+        return response($response,201);
+ 
+    }
+
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|string|confirmed'
+ 
+        ]); 
+ 
+        $user= User::create([
+            'name'=>$fields['name'],
+            'email'=>$fields['email'],
+            'password'=>bcrypt($fields['password'])
+ 
+        ]);
+ 
+        $token = $user->createToken('mytoken')->plainTextToken;
+ 
+        $response = [
+            'user'=>$user,
+            'token'=>$token,
+            'password'=>bcrypt($fields['name'])
+         ];
+ 
+ 
+        return response($response,201);
+
+    }
+    public function logout (Request $request){
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message'=>'Logged out'
+        ];
+    }
+
+}
---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'name'=>'required',
+            'slug'=>'required',
+            'price'=>'required'
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Product::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+        if(Product::destroy($id)){
+            return 'Delete Successful';
+        }else{
+            return 'Delete UnSuccessful';
+        }
+    }
+
+    /**
+     * Search resource from storage.
+     *
+     * @param  String  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        $product = Product::where('name','like','%'.$name.'%')->get();
+        if(sizeof($product) == 0 ){
+            return 'Item Not Found';
+        }else{
+            return $product; 
+        }
+    }
+}
